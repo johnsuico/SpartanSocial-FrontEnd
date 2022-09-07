61b9253aa7a212d0449908bd2b33d6d7e18a7d52
John Suico <johncdsuico@gmail.com>
2022-09-07 09:02:38 -0700

    setup base website structure

---
+++ src/Components/Landing/landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Landing() {
+  return (
+    <div>
+      <h1>Temp Landing Page</h1>
+      <h3>Navigation</h3>
+      <Link to="/login"> Login Page </Link>
+    </div>
+  )
+}
