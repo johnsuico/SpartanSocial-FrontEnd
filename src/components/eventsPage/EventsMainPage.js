449ef54586a5dbc4044761934f71ff4696f725ce
John Suico <johncdsuico@gmail.com>
2022-11-21 14:10:14 -0800

    Rerender when event changes

--- src/components/eventsPage/EventsMainPage.js
+++ src/components/eventsPage/EventsMainPage.js
@@ -37,7 +37,7 @@ export default function EventsMainPage() {
       .catch(err => {
         console.log(err);
       })
-  }, [])
+  }, [events])
 
   function createEventPage() {
     if (isLogged) {
